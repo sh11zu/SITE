c815a6b5b5da6190e2580a80abe2d99161cd68c3
Matt5842 <masneufmathis@gmail.com>
2024-08-16 20:15:08 +0200

    Update homepage

--- STATIC/JS/index.js
+++ STATIC/JS/index.js
@@ -1,3 +1,18 @@
 document.addEventListener('DOMContentLoaded', () => {
     console.log("Homepage loaded");
 });
+
+var modal = document.getElementById("myModal");
+var btn = document.getElementById("openModal");
+var span = document.getElementById("closeModal");
+btn.onclick = function() {
+    modal.style.display = "block";
+    }
+span.onclick = function() {
+    modal.style.display = "none";
+    }
+window.onclick = function(event) {
+    if (event.target == modal) {
+        modal.style.display = "none";
+        }
+    }
